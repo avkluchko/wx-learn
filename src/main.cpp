3c872f9a459b589373064ddcefa7a1ce811a6cf3
Aleksandr Klyuchko <avkluchko@yandex.ru>
2021-02-05 17:14:43 +0500

    Added wxTemplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,44 @@
+// wxWidgets "Hello world" Program
 // For compilers that support precompilation, includes "wx/wx.h".
 #include <wx/wxprec.h>
-
 #ifndef WX_PRECOMP
-
 #include <wx/wx.h>
-
 #endif
-
-class MyApp : public wxApp {
+class MyApp : public wxApp
+{
 public:
     virtual bool OnInit();
 };
-
-class MyFrame : public wxFrame {
+class MyFrame : public wxFrame
+{
 public:
-    MyFrame();
+    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
 
 private:
     void OnHello(wxCommandEvent &event);
-
     void OnExit(wxCommandEvent &event);
-
     void OnAbout(wxCommandEvent &event);
+    wxDECLARE_EVENT_TABLE();
 };
-
-enum {
+enum
+{
     ID_Hello = 1
 };
-
-wxIMPLEMENT_APP(MyApp);
-
-bool MyApp::OnInit() {
-    MyFrame *frame = new MyFrame();
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(ID_Hello, MyFrame::OnHello)
+        EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+            EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+                wxEND_EVENT_TABLE()
+                    wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
     frame->Show(true);
     return true;
 }
-
-MyFrame::MyFrame()
-        : wxFrame(NULL, wxID_ANY, "Hello World") {
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size)
+{
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
                      "Help string shown in status bar for this menu item");
@@ -51,20 +52,17 @@ MyFrame::MyFrame()
     SetMenuBar(menuBar);
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
 }
-
-void MyFrame::OnExit(wxCommandEvent &event) {
+void MyFrame::OnExit(wxCommandEvent &event)
+{
     Close(true);
 }
-
-void MyFrame::OnAbout(wxCommandEvent &event) {
-    wxMessageBox("This is a wxWidgets Hello World example",
+void MyFrame::OnAbout(wxCommandEvent &event)
+{
+    wxMessageBox("This is a wxWidgets' Hello world sample",
                  "About Hello World", wxOK | wxICON_INFORMATION);
 }
-
-void MyFrame::OnHello(wxCommandEvent &event) {
+void MyFrame::OnHello(wxCommandEvent &event)
+{
     wxLogMessage("Hello world from wxWidgets!");
 }
